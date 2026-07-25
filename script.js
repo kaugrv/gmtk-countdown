547fbc368c420cb19598993718274e325e5a0e39
Beans <kau.grv@gmail.com>
2026-07-25 16:06:16 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const globalCountdown = 600 * 5;
+
+let mainGame = document.querySelector(".main-game");
+
+// Pour gérer timeout facilement - https://www.sitepoint.com/delay-sleep-pause-wait/
+function sleep(ms) {
+  return new Promise((fonction) => setTimeout(fonction, ms));
+}
+
+//   // Timer
+//   let seconds = globalCountdown;
+//   let timer = document.querySelector("#timer");
+
+//   const timerInterval = setInterval(() => {
+//     seconds--;
+
+//     timer.innerText = (seconds % 60).toString();
+
+//     if (seconds <= 5) {
+//       timer.style.color = "red";
+//     }
+
+//     if (seconds === 0) {
+//       clearInterval(timerInterval);
+//       endGame();
+//     }
+//   }, 1000);
+
+//   initObjects();
+//   renderObjects();
+// }
+
+//exemple : 
+//   sleep(1500).then(() => {
+//     mainGame.style.display = "flex";
+//     mainGame.style.animation = "apparition 1.5s 1";
+//     introduction.style.display = "none";
+//   });
